69a0eeeac873e77f94dd5952c0cb7b40e574b4e1
kallal103 <kallal.nsu103@gmail.com>
2020-07-16 22:11:18 +0600

    raw sql querries

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index(){
+        // DB::insert('insert into users(name,email,password)values(?,?,?)',[
+        //   'kallal test','kallal@test.com', 'password'  
+        // ]);
+        //DB::update('update users set name = ? where id = 1',['VISER X']);
+
+        //$users = DB::select('select * from users');
+        //return $users;
+        //DB::delete('delete from users where id = 4');
         return view('home');
     }
 }
