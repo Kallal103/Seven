e14d67a8bb126ad2db78234617c7ccb1c5942db2
kallal103 <kallal.nsu103@gmail.com>
2020-07-31 20:41:49 +0600

    Accessor and Mutators

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index(){
+        $data = [
+            'name' => 'VISER X',
+            'email' => 'viserx@test.com',
+            'password' =>'password'
+        ];
+        //User::create($data);
+        // $user = new User();
+        // $user->name = 'Kallal';
+        // $user->email = 'Kallal@viserx.com';
+        // $user->password = bcrypt('password');
+        // $user->save();
+        //User::where('id',8)->update(['name'=>'kallal test']);
+
+        $user = User::all();
+        return $user;
+
+        //User::where('id',6)->delete();
+
+
+        //dd($user);
+
+        
         // DB::insert('insert into users(name,email,password)values(?,?,?)',[
         //   'kallal test','kallal@test.com', 'password'  
         // ]);
