903cbf12f7241651a7a8da22af15bdd89b58b765
kallal103 <kallal.nsu103@gmail.com>
2020-08-05 18:47:15 +0600

    Todo list views create

---
+++ resources/views/todos/create.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class=" text-center pt-10">
+            <h1 class="text-2xl">What you need to do</h1>
+    <form action="/todos/create" method="POST" class="py-5">
+        @csrf
+        <input type="text" name="title" class=" py-2 px-2 border rounded">
+        <input type="submit" value="create" class=" p-2 border rounded">
+
+    </form>
+    </div>
+
+</body>
+</html>
