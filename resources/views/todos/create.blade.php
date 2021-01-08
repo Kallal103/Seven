42cc997a5a98bcbbea9656c61ed095f3fab1034f
kallalnsu103 <kallal.das@northsouth.edu>
2021-01-09 03:10:09 +0600

    Show all todo

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Todo</title>
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-</head>
-<body>
-    <div class=" text-center pt-10">
-            <h1 class="text-2xl">What you need to do</h1>
+@extends('todos.layout')
+
+@section('content')
+
+<h1 class="text-2xl">What you need to do</h1>
             <x-alert />
     <form action="/todos/create" method="POST" class="py-5">
         @csrf
@@ -17,7 +10,5 @@
         <input type="submit" value="create" class=" p-2 border rounded">
 
     </form>
-    </div>
 
-</body>
-</html>
+@endsection
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,1 +1,18 @@
-All todos
+@extends('todos.layout')
+@section('content')
+
+    <h1 class="text-2xl">All todos</h1>
+    <ul>
+        @foreach ($todos as $todo)
+        <li>
+            {{$todo->title}}
+        </li>
+        @endforeach
+    </ul>
+@endsection
+
+
+
+
+
+
